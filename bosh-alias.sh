486358e594d1d7109ed596240ad5d291baa6353e
flee999 <flee999@hotmail.com>
2019-01-12 13:26:28 -0500

    custom zookeeper manifest

--- bosh-alias.sh
+++ bosh-alias.sh
@@ -1,3 +1,4 @@
+bosh alias-env vbox -e 192.168.50.6 --ca-cert <(bosh int ./creds.yml --path /director_ssl/ca)
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=$(bosh int ./creds.yml --path /admin_password)
-export BOSH_ENVIRONMENT=bosh
+export BOSH_ENVIRONMENT=vbox
